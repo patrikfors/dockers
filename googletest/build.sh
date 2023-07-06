c0f221d0c6c860841fb92dd5ddd3ec4a0253f983
Patrik Fors <patrik.fors@gmail.com>
2023-07-06 16:50:24 +0200

    Created an image for build environment and an image for building googletest

---
+++ googletest/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+if [[ -z $1 ]]; then
+        branch=main
+        release=googletest
+else
+        branch=v$1.x
+        release=googletest-$1
+fi
+
+echo branch=$branch
+echo release=$release
+
+git clone --branch $branch https://github.com/google/googletest.git
+mkdir build && cd build
+
+cmake -G 'Ninja Multi-Config' -DCMAKE_INSTALL_PREFIX=/app/install/googletest /app/googletest
+cmake --build . --config Release --target install;
+cd /app/install;
+
+tar cvfz /export/$release.tar.gz googletest
+
+echo "Exported $release.tar.gz"
